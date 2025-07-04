158d519ef1fa65487210126768b8ae4749c63e36
Aziz <sobirjonov0909@gmail.com>
2025-07-04 19:53:36 +0500

    aziz

---
+++ myproject/myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'myapp/index.html')
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
+def skills(request):
+    return render(request, 'myapp/skills.html')
+
+def contact(request):
+    return render(request, 'myapp/contact.html')
+
